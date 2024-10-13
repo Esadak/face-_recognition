e5328940df1ccdcef1ab8723c80913cf7e686155
Ezat Akbari <ezatakbari23@hotmail.com>
2024-10-13 20:11:01 +0200

    Maked some changes and add function to the program

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import threading
 import cv2
-from deepface import Deepface
+from deepface import DeepFace
 
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIDHT, 480)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 853)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
 
 counter = 0 
 face_match = False
 refrence_img = cv2.imread("reference.jpg")
 
-def check_face(fram):
-    pass
+def check_face(frame):
+    global face_match
+    try:
+        if DeepFace.verify(frame, refrence_img.copy())['verified']:
+            face_match = True
+        else:
+            face_match = False
+
+    except ValueError:
+        face_match = False
+
+
 while True:
     ret, frame = cap.read()
 
@@ -27,7 +38,12 @@ while True:
         counter += 1
 
         if face_match:
-            cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0))
+            cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+
+        else:
+            cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+
+        cv2.imshow("Video", frame)
 
 
     key = cv2.waitKey(1)
